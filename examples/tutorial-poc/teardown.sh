21976e2491ae080973fc7c007631c00c1313c3fc
Alex Creasy <alex@creasy.dev>
2026-07-29 15:20:06 +0100

    Adds the practical elements for lesson 1 of the GitOps tutorial series.
    Signed-off-by: Alex Creasy <alex@creasy.dev>

--- examples/tutorial-poc/teardown.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-CLUSTER_NAME="gitops-tutorial"
-
-echo "Deleting KinD cluster '${CLUSTER_NAME}'..."
-kind delete cluster --name "${CLUSTER_NAME}"
-echo "Done. All tutorial resources have been removed."
