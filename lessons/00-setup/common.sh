88863871e0d7bee32545d895dc4d85c0486d6887
Alex Creasy <alex@creasy.dev>
2026-08-03 16:55:37 +0100

    Refactor common variables in lesson 2 into common script
    Signed-off-by: Alex Creasy <alex@creasy.dev>

--- lessons/00-setup/common.sh
+++ lessons/00-setup/common.sh
@@ -13,3 +13,6 @@ GITEA_URL="http://localhost:${GITEA_HOST_PORT}"
 # KAFKA_NAMESPACE is also the namespace the Strimzi operator is configured to watch.
 KAFKA_CLUSTER_NAME="my-cluster"
 KAFKA_NAMESPACE="kafka-tutorial"
+
+KAFKA_STAGING_NAMESPACE="kafka-staging"
+KAFKA_PRODUCTION_NAMESPACE="kafka-production"
